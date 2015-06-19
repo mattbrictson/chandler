67b0f02c9f6765b03ab74b3599869f098b42744b
Matt Brictson <matt@mattbrictson.com>
2015-06-19 16:05:40 -0700

    Coveralls needs to load before minitest/autorun

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,9 +1,9 @@
 $LOAD_PATH.unshift(File.expand_path("../../lib", __FILE__))
 
-require "minitest/autorun"
-
 # Coveralls has to be loaded first
 require_relative("./support/coveralls")
 
 # Load everything else from test/support
 Dir[File.expand_path("../support/**/*.rb", __FILE__)].each { |rb| require(rb) }
+
+require "minitest/autorun"
