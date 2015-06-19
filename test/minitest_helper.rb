b7c1ff3a723c3cfba54f2cc4c76d67ea7ab29e32
Matt Brictson <matt@mattbrictson.com>
2015-06-19 11:12:33 -0700

    Eager load to improve code coverage measurement

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -4,6 +4,9 @@ $LOAD_PATH.unshift(chandler_lib)
 if ENV["TRAVIS"]
   require "coveralls"
   Coveralls.wear!
+  # Eager load the entire lib directory so that SimpleCov is able to report
+  # accurate code coverage metrics.
+  at_exit { Dir["#{chandler_lib}/**/*.rb"].each { |rb| require(rb) } }
 end
 
 require "minitest/autorun"
