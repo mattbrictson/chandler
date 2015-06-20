8785cf77badb714e84caf33ad4621389bdbee408
Matt Brictson <matt@mattbrictson.com>
2015-06-20 15:10:06 -0700

    Get rid of unnecessary included..instance_exec

--- lib/chandler/logging.rb
+++ lib/chandler/logging.rb
@@ -3,15 +3,11 @@ module Chandler
   # logging methods by delegating to the configured Logger.
   #
   module Logging
-    def self.included(target)
-      target.instance_exec do
-        extend Forwardable
-        private def_delegator :config, :logger
-        private def_delegator :logger, :benchmark
-        private def_delegator :logger, :debug
-        private def_delegator :logger, :error
-        private def_delegator :logger, :info
-      end
-    end
+    extend Forwardable
+    private def_delegator :config, :logger
+    private def_delegator :logger, :benchmark
+    private def_delegator :logger, :debug
+    private def_delegator :logger, :error
+    private def_delegator :logger, :info
   end
 end
