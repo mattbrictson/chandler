e62555c9be7e29ab9f7a2293ba045540b7611a89
Matt Brictson <matt@mattbrictson.com>
2015-06-20 14:53:53 -0700

    Don't report coverage metrics of test/**

--- test/support/coveralls.rb
+++ test/support/coveralls.rb
@@ -1,6 +1,13 @@
 if ENV["TRAVIS"]
+  require "simplecov"
   require "coveralls"
-  Coveralls.wear!
+
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.start do
+    # No need to report coverage metrics for the test code
+    add_filter "test"
+  end
+
   # Eager load the entire lib directory so that SimpleCov is able to report
   # accurate code coverage metrics.
   chandler_lib = File.expand_path("../../../lib", __FILE__)
