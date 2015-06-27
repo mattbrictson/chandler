f81391c7b9bc1a2828e48e8fd1344f419f4ffcde
Matt Brictson <matt@mattbrictson.com>
2015-06-26 19:50:08 -0700

    Add "integration" test that uses exe/chandler

---
+++ test/exe_test.rb
@@ -0,0 +1,19 @@
+require "minitest_helper"
+
+class ExeTest < Minitest::Test
+  def test_chandler_is_executable_and_exits_with_success
+    within_project_root do
+      Bundler.with_clean_env do
+        output = `bundle exec chandler --version`
+        assert_equal("chandler version #{Chandler::VERSION}\n", output)
+        assert($CHILD_STATUS.success?)
+      end
+    end
+  end
+
+  private
+
+  def within_project_root(&block)
+    Dir.chdir(File.expand_path("../..", __FILE__), &block)
+  end
+end
